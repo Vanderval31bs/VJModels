2c556f903dc0de7bdea12f8b134406e96d1e2ee0
Vanderval <vanderval31bs@hotmail.com>
2024-08-15 13:13:38 -0300

    Start repo

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'VJModels',         # How you named your package folder (MyLib)
+  packages = ['VJModels'],   # Chose the same as "name"
+  version = '0.1.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'My person machine learning models',   # Give a short description about your library
+  author = 'Vanderval Borges de Souza Junior',                   # Type in your name
+  author_email = 'vander31bs@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/Vanderval31bs/VJModels',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Vanderval31bs/VJModels/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['MachineLearning', 'Models', 'Forests'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.11',      #Specify which pyhton versions that you want to support
+  ],
+)
